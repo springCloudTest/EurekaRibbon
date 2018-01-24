799a79324cfdedf27cfb8e4e0412da204d766f67
zhengxiaobin <hubert@12138gmail.com>
2018-01-25 01:19:48 +0800

    first

---
+++ src/main/java/com/hubert/EurekaService/Controller/ConsumerController.java
@@ -0,0 +1,30 @@
+package com.hubert.EurekaService.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 调用服务提供方的服务
+ */
+@RestController
+public class ConsumerController {
+
+    //通过直接RestTemplate来调用服务 rest
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        ResponseEntity response = restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class);
+
+        return response.getBody().toString();
+    }
+
+
+}
+
